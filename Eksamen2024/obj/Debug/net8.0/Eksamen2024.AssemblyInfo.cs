16a59224680b1cb3b46a1738be95b3b9b573ab12
SirWeekend <115225208+SirWeekend@users.noreply.github.com>
2024-09-19 11:50:07 +0200

    Lagre endringene fra Ajax-Call

--- Eksamen2024/obj/Debug/net8.0/Eksamen2024.AssemblyInfo.cs
+++ Eksamen2024/obj/Debug/net8.0/Eksamen2024.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Eksamen2024")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc7b1fccfc07281bc2d0b55b1505b52e7b8afbf7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aab78addba8259cb994da46923cde43ca5728c8b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Eksamen2024")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Eksamen2024")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
