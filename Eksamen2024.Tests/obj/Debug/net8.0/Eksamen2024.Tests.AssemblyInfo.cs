2959f3284d8cb3188948e8dcc930a2bea2966de2
bgwtb <97193606+bgwtb@users.noreply.github.com>
2024-10-03 13:45:35 +0200

    startup commits, fix gitignore

--- Eksamen2024.Tests/obj/Debug/net8.0/Eksamen2024.Tests.AssemblyInfo.cs
+++ Eksamen2024.Tests/obj/Debug/net8.0/Eksamen2024.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Eksamen2024.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e325e39e5687b50d29a8db98a7063717f9b83c1d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c01294b98eda991e4670c255d4d21d4bd85d7f14")]
 [assembly: System.Reflection.AssemblyProductAttribute("Eksamen2024.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Eksamen2024.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
