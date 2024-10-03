d341f4c381c15b07ba7b4ed6d19c6947bf3b4cc5
bgwtb <97193606+bgwtb@users.noreply.github.com>
2024-10-03 10:49:17 +0200

    stash commits

--- Eksamen2024.Tests/obj/Debug/net8.0/Eksamen2024.Tests.AssemblyInfo.cs
+++ Eksamen2024.Tests/obj/Debug/net8.0/Eksamen2024.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Eksamen2024.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3849b732011b4313d9191e99a435fcb89be11f1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d682bb7249eb61aaae44ea4fba4a61d0101e25af")]
 [assembly: System.Reflection.AssemblyProductAttribute("Eksamen2024.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Eksamen2024.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
