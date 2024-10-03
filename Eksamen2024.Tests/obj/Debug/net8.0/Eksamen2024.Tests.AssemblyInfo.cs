5590c67c4366514fc2614dc41a42e20b3fe233c9
bgwtb <97193606+bgwtb@users.noreply.github.com>
2024-10-03 10:15:50 +0200

    startup commits

--- Eksamen2024.Tests/obj/Debug/net8.0/Eksamen2024.Tests.AssemblyInfo.cs
+++ Eksamen2024.Tests/obj/Debug/net8.0/Eksamen2024.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Eksamen2024.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e3be385cf3207c05b5d5c0631763048ade5db18e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+377be6ecbd8c8454486299d0c35837876f8ce1b0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Eksamen2024.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Eksamen2024.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
