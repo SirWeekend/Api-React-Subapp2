07091878e6640c11ea6b541bd86c00828f019eb2
FredrikBjorgan <fredrik_bjorgan@hotmail.com>
2024-10-17 14:02:44 +0200

    Fikset Logg inn og meny knapp
    Fikset slikt at Logg inn knappen linker til Login.cshtml og Meny knappen linker til Hjem skjermen

--- Eksamen2024/obj/Debug/net8.0/Eksamen2024.AssemblyInfo.cs
+++ Eksamen2024/obj/Debug/net8.0/Eksamen2024.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Eksamen2024")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+711d8f72aefd8e1b4d0cfe566aa42d10b60e42df")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9dddf43139f6aa2f69b1c52c7a9a246efd2864b2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Eksamen2024")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Eksamen2024")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
