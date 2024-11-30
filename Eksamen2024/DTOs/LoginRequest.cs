5d37f73adfed713e97f9aaa8d27a4d23894ae6b9
SirWeekend <115225208+SirWeekend@users.noreply.github.com>
2024-11-30 12:42:17 +0100

    Fixed warning regarding login og register

--- Eksamen2024/DTOs/LoginRequest.cs
+++ Eksamen2024/DTOs/LoginRequest.cs
@@ -2,7 +2,7 @@ namespace Eksamen2024.DTOs
 {
     public class LoginRequest
     {
-        public string Username { get; set; }
-        public string Password { get; set; }
+        public required string Username { get; set; }
+        public required string Password { get; set; }
     }
 }
